474882e1ad5a2c6390fc196235bd8e43fc948002
Uhtred2 <134827450+Uhtred2@users.noreply.github.com>
2023-05-28 02:49:41 -0400

    Started my Money Converter script

---
+++ money.py
@@ -0,0 +1,11 @@
+"""
+Converts currency values
+"""
+
+def ConvertToDollars(float):
+#ConversionMaths
+    return float
+def main():
+    euros = input("please enter amount of Euros")
+    ConvertToDollars(euros)  
+    return
